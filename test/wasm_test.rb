8338946caf23f29d197dedf44934bec20320a013
Atsushi Takayama <taka.atsushi@gmail.com>
2018-08-14 09:30:48 +0900

    Refactor: use compiled wasm for test

--- test/wasm_test.rb
+++ test/wasm_test.rb
@@ -1,35 +1,14 @@
 require "test_helper"
 
 class WasmTest < Minitest::Test
-  def test_compile_add
-    # (module
-    #  (func $add (export "add") (param $lhs i32) (param $rhs i32) (result i32)
-    #   get_local $lhs
-    #   get_local $rhs
-    #   i32.add)
-    # )
-    a = [
-      0x00, 0x61, 0x73, 0x6d, # magic
-      0x01, 0x00, 0x00, 0x00, # version
-      # type section
-      0x01, 0x07, 0x01, 0x60, 0x02, 0x7f, 0x7f, 0x01, 0x7f,
-      # function section
-      0x03, 0x02, 0x01, 0x00,
-      # export section
-      0x07, 0x07, 0x01, 0x03, 0x61, 0x64, 0x64, 0x00, 0x00,
-      # code section
-      0x0a, 0x09, 0x01, 0x07, 0x00, 0x20, 0x00, 0x20, 0x01, 0x6a, 0x0b,
-    ]
-    instance = compile(a)
+  def test_add
+    open("#{__dir__}/data/add.wasm") do |f|
+      klass = Wagyu::Wasm.compile_streaming(f)
+      instance = Struct.new(:exports).new(klass.new)
 
-    result = instance.exports.add(1, 2)
-    assert_equal(3, result)
-  end
-
-  def compile(a)
-    binary = a.pack('C*')
-    klass = Wagyu::Wasm.compile(binary)
-    Struct.new(:exports).new(klass.new)
+      result = instance.exports.add(1, 2)
+      assert_equal(3, result)
+    end
   end
 
   def test_square
